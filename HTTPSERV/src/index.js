55d3e7e66db2b3bb4269d929dbc2a8c10fd4d6d9
Juubes <grumpt1@gmail.com>
2022-11-13 01:38:40 +0200

    Install node dependencies

--- HTTPSERV/src/index.js
+++ HTTPSERV/src/index.js
@@ -6,6 +6,7 @@ const app = express();
 app.use(express.text());
 
 app.get("*", async (req, res) => {
+  // I'm fetching the data trough HTTP instead of a volume. The requirements don't mind and it's more scalable :-)
   const response = await fetch("http://obse:8080/");
   const data = await response.text();
 
