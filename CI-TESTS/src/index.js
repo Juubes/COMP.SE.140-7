8634362d7939cd5f59f2763eba2b95072cc35fdf
Juubes <grumpt1@gmail.com>
2022-11-30 15:24:43 +0200

    Different docker compose file for CI

--- CI-TESTS/src/index.js
+++ CI-TESTS/src/index.js
@@ -1,2 +1,17 @@
-const res = await fetch("http://HTTPSERV:8080/");
-const data = await res.text();
+const poll = async () => {
+  const res = await fetch("http://HTTPSERV:8080/");
+  const data = await res.text();
+
+  console.log("Tested. Data: ");
+  console.log(data);
+
+  const lines = data.split("\n");
+
+  // Doesn't test the dates
+  const result = lines.some((line) => {});
+  if (result) {
+    console.log("Correct data");
+  }
+};
+
+setInterval(poll, 1000);
